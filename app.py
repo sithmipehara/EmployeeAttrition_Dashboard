a5ce604394f7ab4129ec3b7f64c272f6776181fd
sithmipehara <sithmiwijesinghe20@gmail.com>
2024-10-15 21:12:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,19 +36,19 @@ with col1:
                 unsafe_allow_html=True)
 
 with col2:
-    st.markdown(f"<div style='background-color: {background_color}; padding: 20px; border-radius: 10px;'>"
+    st.markdown(f"<div style='background-color: {background_color}; padding: 20px; border-radius: 10px;height: 150px'>"
                 f"<h5 style='text-align: center; color: #FFFFFF;'>Number of Categorical Variables</h5>"
                 f"<h2 style='text-align: center; color: #FFFFFF;'>{num_categorical_vars}</h2></div>", 
                 unsafe_allow_html=True)
 
 with col3:
-    st.markdown(f"<div style='background-color: {background_color}; padding: 20px; border-radius: 10px;'>"
+    st.markdown(f"<div style='background-color: {background_color}; padding: 20px; border-radius: 10px;height: 150px'>"
                 f"<h5 style='text-align: center; color: #FFFFFF;'>Number of Numerical Variables</h5>"
                 f"<h2 style='text-align: center; color: #FFFFFF;'>{num_numerical_vars}</h2></div>", 
                 unsafe_allow_html=True)
 
 with col4:
-    st.markdown(f"<div style='background-color: {background_color}; padding: 20px; border-radius: 10px; height: 200px'>"
+    st.markdown(f"<div style='background-color: {background_color}; padding: 20px; border-radius: 10px; height: 150px'>"
                 f"<h5 style='text-align: center; color: #FFFFFF;'>Response Variable</h5>"
                 f"<h2 style='text-align: center; color: #FFFFFF;'>{response_variable}</h2></div>", 
                 unsafe_allow_html=True)
