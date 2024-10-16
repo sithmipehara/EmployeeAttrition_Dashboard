f9ee1e9148fc45ba6133be181c8fce82e0c76b5b
sithmipehara <sithmiwijesinghe20@gmail.com>
2024-10-16 18:26:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,8 @@ with col1:
 # Second column: Three containers for donut chart and response details
 with col2:
     # Container for Donut Chart
-    st.markdown("<div class='container'><h4 style='text-align: center;'>Attrition Distribution</h4>", unsafe_allow_html=True)
+    st.markdown("<div class='container'> , unsafe_allow_html=True)
+    st.markdown("<h4 style='text-align: center;'>Attrition Distribution</h4>")
     
     attrition_counts = df['Attrition'].value_counts().reset_index()
     attrition_counts.columns = ['Attrition', 'Count']
